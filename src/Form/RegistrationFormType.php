184ebe04a94ddc5efc566df57f9e06c19ba56272
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2025-04-15 10:12:23 +0200

    Updated register form validation rules

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,20 +4,29 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
   public function buildForm(FormBuilderInterface $builder, array $options): void
   {
     $builder
-      ->add('email')
+      ->add('email', EmailType::class, [
+        'label' => 'Email',
+        'constraints' => [
+          new NotBlank(['message' => 'Please enter a email address.']),
+          new Email(['message' => 'Please enter a valid email address.']),
+        ],
+      ])
       ->add('agreeTerms', CheckboxType::class, [
         'mapped' => false,
         'constraints' => [
@@ -26,21 +35,31 @@ class RegistrationFormType extends AbstractType
           ]),
         ],
       ])
-      ->add('plainPassword', PasswordType::class, [
+      ->add('plainPassword', RepeatedType::class, [
         // instead of being set onto the object directly,
         // this is read and encoded in the controller
+        'type' => PasswordType::class,
         'mapped' => false,
+        'invalid_message' => 'The password fields must match.',
         'attr' => ['autocomplete' => 'new-password'],
+        'first_options' => [
+          'label' => 'Password',
+          'mapped' => false
+        ],
+        'second_options' => [
+          'label' => 'Repated password',
+          'mapped' => false
+        ],
         'constraints' => [
           new NotBlank([
-            'message' => 'Please enter a password',
+            'message' => 'Please enter a password.',
           ]),
           new Length([
             'min' => 6,
-            'minMessage' => 'Your password should be at least {{ limit }} characters',
+            'minMessage' => 'Your password should be at least {{ limit }} characters.',
             // max length allowed by Symfony for security reasons
             'max' => 4096,
-          ]),
+          ])
         ],
       ])
     ;
